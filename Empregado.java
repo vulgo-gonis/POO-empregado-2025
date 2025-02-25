c77c80bf352ae6a4da2bf2aaa6456df0518f8dbe
Lucas Quadros das Dores <25.00107-4@maua.br>
2025-02-25 09:15:17 -0300

    Cálculo de salário efetuado de acordo com o tipo

--- Empregado.java
+++ Empregado.java
@@ -1,7 +1,12 @@
 public class Empregado {
     private String nome;
     private int idade;
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
     
+
     public String getNome() {
         return nome;
     }
@@ -13,6 +18,30 @@ public class Empregado {
     }
     public void setIdade(int idade) {
         this.idade = idade;
+    }
+    public int getTipo() {
+        return tipo;
+    }
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+    public double getSalario() {
+        return salario;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    public double getComissao() {
+        return comissao;
+    }
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+    public double getBonus() {
+        return bonus;
+    }
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
     }
 
     
