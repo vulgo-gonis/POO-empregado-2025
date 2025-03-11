c9944d53c6fb3b74aa8e8dbc7e980f0e3d8c2e7f
Lucas Quadros das Dores <25.00107-4@maua.br>
2025-03-11 08:46:12 -0300

    alteração do sálario

--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -9,13 +9,13 @@ public class TesteEmpregado {
         emp1.setTipo(1);
         emp2.setTipo(2);
         emp3.setTipo(3);
-
-        emp1.setSalario(2000);
+        //configurando algum valor 
+        emp1.setSalario(5000);
         emp2.setSalario(1700);
-        emp2.setComissao(0.2);
+        emp2.setComissao(0.2); // 20% de comissão sobre o sálario 
         emp3.setSalario(1500);
         emp3.setBonus(400);
-
+        //mostrar valores 
         System.out.println(emp1.CalculaSalario());
         System.out.println(emp2.CalculaSalario());
         System.out.println(emp3.CalculaSalario());
